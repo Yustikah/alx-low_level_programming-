1a194a1edc48c1be91793c78f5166d953c0e4502
Ymma <avoulouyustikah@gmail.com>
2022-04-18 11:22:36 -0700

    Changes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,7 @@
 /**
  * print_times_table - prints the times table
  * @n: integer for which the times table will be printed
- * Description: prints the time table 
+ * Description: prints the time table
  * Return: void
  */
 
