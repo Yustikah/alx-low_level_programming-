c97aeb093f36239dcd409c0792d7abef86a01ce8
Yustikah <99501573+Yustikah@users.noreply.github.com>
2022-04-29 23:10:53 +0100

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,15 @@
 #include "main.h"
-
 /**
  * cap_string - capitalizes all words of a string
- * @s: input string
- * Return: the pointer to destination
+ * @s: input string.
+ * Return: the pointer to dest.
  */
 
 char *cap_string(char *s)
 {
 	int count = 0, i;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
 	count++;
@@ -20,7 +20,7 @@ char *cap_string(char *s)
 			if (*(s + count) == sep_words[i])
 			{
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-						*(s + (count + 1)) = *(s + (count + 1)) - 32;
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
 		}
@@ -28,4 +28,3 @@ char *cap_string(char *s)
 	}
 	return (s);
 }
-
