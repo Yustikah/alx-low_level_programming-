af487cb959ac64425185341f8f4f9ee681f7d52e
Yustikah <99501573+Yustikah@users.noreply.github.com>
2022-05-21 00:32:38 +0100

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - prints the name of the file program was compiled from
- * Return: Always 0
- */
+ * main - prints the name of the file it was compiled from.
+ *
+ * Return: nothing.
+**/
 
 int main(void)
 {
